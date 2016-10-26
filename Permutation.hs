fc8124eb535d335cd3707a6448b306f269ed2eea
raymoo <uguu@installgentoo.com>
2016-10-26 12:31:48 -0700

    prove prop things

--- Permutation.hs
+++ Permutation.hs
@@ -2,6 +2,10 @@ module Permutation where
 
 import qualified Data.Set as S
 
+type Proof = ()
+trivial :: Proof
+trivial = ()
+
 -- Not a sufficient condition for something to be a permutation, but we can work
 -- with this for now.
 {-@ predicate Permutation X Y = (listElts X = listElts Y) && (len X = len Y)  @-}
@@ -16,9 +20,19 @@ notPerm = ([1,1], [1,2])
 goodPerm :: ([Int], [Int])
 goodPerm = ([1, 2, 3, 4], [3, 4, 2, 1])
 
+{-@ equalLengths :: xs:[a] -> ys:[a] -> { p:Proof | Permutation xs ys }
+                 -> { q:Proof | len xs = len ys } @-}
+equalLengths :: [a] -> [a] -> Proof -> Proof
+equalLengths _ _ _ = trivial
+
+{-@ commutative :: xs:[a] -> ys:[a] -> { _:Proof | Permutation xs ys }
+                 -> { _:Proof | Permutation ys xs } @-}
+commutative :: [a] -> [a] -> Proof -> Proof
+commutative _ _ _ = trivial
+
 -- Things to prove
 
--- The lengths of permutations are equal: Not done
--- Permutation is commutative: Not done
+-- The lengths of permutations are equal: Done
+-- Permutation is commutative: Done
 -- [1,1] is not a permutation of [1,2]: Done (fails as expected)
 -- [1,2,3,4] is a permutation of [3,4,2,1]: Done
