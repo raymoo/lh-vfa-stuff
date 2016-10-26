14cb78bd4565dc6ac6e26ed7b67347d80ce69bef
raymoo <uguu@installgentoo.com>
2016-10-26 12:19:54 -0700

    permutation examples appear to work now

--- Permutation.hs
+++ Permutation.hs
@@ -1,5 +1,7 @@
 module Permutation where
 
+import qualified Data.Set as S
+
 -- Not a sufficient condition for something to be a permutation, but we can work
 -- with this for now.
 {-@ predicate Permutation X Y = (listElts X = listElts Y) && (len X = len Y)  @-}
@@ -18,5 +20,5 @@ goodPerm = ([1, 2, 3, 4], [3, 4, 2, 1])
 
 -- The lengths of permutations are equal: Not done
 -- Permutation is commutative: Not done
--- [1,1] is not a permutation of [1,2]: Broken?
--- [1,2,3,4] is a permutation of [3,4,2,1]: Broken?
+-- [1,1] is not a permutation of [1,2]: Done (fails as expected)
+-- [1,2,3,4] is a permutation of [3,4,2,1]: Done
